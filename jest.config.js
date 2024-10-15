cb77c31c5949155beecacbabb389ec7def50d511
alonageorge <alonageorge2510@gmail.com>
2024-10-15 13:22:38 +0530

    new changes

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  testEnvironment: "jsdom", // or "node" depending on your setup
-  reporters: [
-    "default",
-    ["jest-junit", { outputDirectory: "reports/jest", outputName: "junit.xml" }],
-    ["jest-html-reporter", {
-      pageTitle: "Test Report",
-      outputPath: "reports/jest-report.html",
-      includeFailureMsg: true,
-      includeConsoleLog: true,
-    }],
-  ],
-};
+
