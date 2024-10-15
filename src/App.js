f7d5429c4ce3f932b095325b626e488b2d775c77
alonageorge <alonageorge2510@gmail.com>
2024-10-15 10:37:19 +0530

    new app

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello world</h1>
+        <h2>This is sample react application for devops testing</h2>
+        <h3>Creating a github action template for react UI app</h3>
       </header>
     </div>
   );
